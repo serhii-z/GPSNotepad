faaed4d29bd87f4ebccb9d084c3c6693076cd229
serhii-z <sergey59070@gmail.com>
2021-03-31 21:34:39 +0300

    Added IEntityBase, UserModel, PinModel.

---
+++ GPSNotepad/GPSNotepad/GPSNotepad/Models/PinModel.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+
+namespace GPSNotepad.Models
+{
+    [Table("Pins")]
+    public class PinModel : IEntityBase
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ GPSNotepad/GPSNotepad/GPSNotepad/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace GPSNotepad.Models
+{
+    [Table("Users")]
+    public class UserModel : IEntityBase
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Unique]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

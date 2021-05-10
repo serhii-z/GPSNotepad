b266003f83cbbce16cf18a4462b523d64f2263da
serhii-z <sergey59070@gmail.com>
2021-05-10 07:13:30 +0300

    Added font.

--- GPSNotepad/GPSNotepad/GPSNotepad/AssemblyInfo.cs
+++ GPSNotepad/GPSNotepad/GPSNotepad/AssemblyInfo.cs
@@ -3,4 +3,3 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("Montserrat.ttf", Alias = "Montserrat")]
-[assembly: ExportFont("Colatemta.otf", Alias = "Colatemta")]
